c8376c84f5a7dbfd5200ee5a832c2d514749eccf
Jacob Pagan <jake2415@gmail.com>
2021-06-29 15:28:31 -0500

    Added the initial model for the app

---
+++ backend/sandwich/tests.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+from .models import Sandwich
+# Create your tests here.
+
+
+class TestSandwich(TestCase):
+
+    def setUp(self):
+        pass
+        
+    def test_sandwich_was_created(self):
+        pass
