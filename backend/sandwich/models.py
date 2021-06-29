bb72c1ae40459bf696e5182712a819e65c3a1243
Jacob Pagan <jake2415@gmail.com>
2021-06-29 15:53:41 -0500

    Added some tests

--- backend/sandwich/models.py
+++ backend/sandwich/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 
-class Sandwich(models.Model):
+class SandwichStore(models.Model):
     sandwich_name = models.CharField(max_length=200, blank=False)
     street = models.CharField(max_length=400, blank=True)
     city = models.CharField(max_length=400, blank=True)
@@ -15,4 +15,7 @@ class Sandwich(models.Model):
     logo_image = models.ImageField(upload_to='sandwichImages', blank=True, default='sandwichImages/sandwichlogo.png')
     email = models.EmailField(max_length=245, blank=True)
     active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
     
--- backend/sandwich/tests.py
+++ backend/sandwich/tests.py
@@ -1,12 +1,22 @@
 from django.test import TestCase
-from .models import Sandwich
+from django.shortcuts import get_object_or_404
+from .models import SandwichStore
 # Create your tests here.
 
 
 class TestSandwich(TestCase):
 
     def setUp(self):
-        pass
+        sandwich = SandwichStore.objects.create(sandwich_name='test sandwich')
         
-    def test_sandwich_was_created(self):
-        pass
+    def test_sandwichstore_was_created(self):
+        get_sandwich = SandwichStore.objects.get(sandwich_name='test sandwich')
+        self.assertIsNotNone(get_sandwich)
+
+    def test_sandwich_was_not_created(self):
+        try:
+            sandwich = SandwichStore.objects.get(sandwich_name="test_sandwich")
+        except SandwichStore.DoesNotExist:
+            sandwich = None
+        self.assertIsNone(sandwich)
+
