1cbdead5f3e03eb4508c978d6829dcde78e651d6
Jacob Pagan <jake2415@gmail.com>
2021-06-30 20:34:05 -0500

    Done for day two

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'sandwich',
 ]
 
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
@@ -114,6 +116,8 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 
 # Static files (CSS, JavaScript, Images)
--- backend/sandwich/serializers.py
+++ backend/sandwich/serializers.py
@@ -1,11 +1,18 @@
 from rest_framework import serializers
 from .models import SandwichStore
-
+from rest_framework.reverse import reverse
 
 class SandwichListSerializer(serializers.ModelSerializer):
+    absolute_url = serializers.SerializerMethodField()
     class Meta:
         model = SandwichStore
-        fields = ['id','sandwich_name','city','zip_code','logo_image']
+        fields = ['id','sandwich_name','city','zip_code','logo_image', 'absolute_url']
+
+    def get_absolute_url(self, obj):
+        #look up why you need a comma after the obj.pk
+        return reverse('sandwich_detail', args=(obj.pk,))
+    
+    
 
 
 
